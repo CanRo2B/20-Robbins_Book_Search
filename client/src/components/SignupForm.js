dd273c39163d362673672a60919f8e5e62fa4b44
Candace Robbins <canrob0522@gmail.com>
2022-06-03 23:30:31 -0500

    playing with signup to make it work

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -32,13 +32,14 @@ const SignupForm = () => {
 
     try {
       const response = await ADD_USER({variables: {...userFormData},});
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
       
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
+      
     } catch (err) {
       console.error(err);
       setShowAlert(true);
